3a7e78729d81a5bc8892589ba4856223fedaed50
Natalia <nef.n@list.ru>
2021-09-15 12:53:20 +0300

    урок 3

--- app/src/main/java/keyone/keytwo/lesson3_2kotlin/lesson3/Lesson3.kt
+++ app/src/main/java/keyone/keytwo/lesson3_2kotlin/lesson3/Lesson3.kt
@@ -1,35 +1,39 @@
 package keyone.keytwo.lesson3_2kotlin.lesson3
 
 import android.app.Application
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import kotlin.random.Random
 
 class Lesson3 {
 
-    fun  mainFirstPart(){
+    fun mainFirstPart() {
 
         val bar = AppCompatActivity().getSupportActionBar()
         val menu = AppCompatActivity().menuInflater
-        val  appNotNullable: Application = AppCompatActivity().application
-        val  appNullable: Application? = AppCompatActivity().application
+        val appNotNullable: Application = AppCompatActivity().application
+        val appNullable: Application? = AppCompatActivity().application
 
 
         //_____-----------
-        var notNullable:String = ""
+        var notNullable: String = ""
 
         //val testObg:Test? = if (Random(2).nextInt()>0) Test() else null
 
         // сделали объект тест nullable типа
         // если testObg? null, то все выражение null
-        val testObg:Test? = Test()
-        var nullable:String? = ""
+        val testObg: Test? = Test()
+        var nullable: String? = ""
 
 
-       // проверка nullable или notNullable
+        // проверка nullable или notNullable
         // оператор элвиса ?:
         notNullable = testObg?.stringTest ?: ""
         // или полная версия
-        notNullable = if (testObg?.stringTest!=null){testObg.stringTest}else{ ""}
+        notNullable = if (testObg?.stringTest != null) {
+            testObg.stringTest
+        } else {
+            ""
+        }
 
         // nullpointerexception выстрелим в ногу, укажем явно, но так лучше не делать, что-бы самому не
         // отслеживать
@@ -38,23 +42,53 @@ class Lesson3 {
 
         //или полная версия
         nullable = null
-        
-        if (nullable!==null){
+
+        if (nullable !== null) {
             nullable!!
         }
 
-        if (nullable!==null){
+        if (nullable !== null) {
 
         }
-//------------------------------------------
-        // коллекции и массивы
-        //
-    fun mainSecondPart(){
+        //------------------------------------------
+        // массивы
+        // массивы создаем через arrayOf()
+        fun mainSecondPart() {
+            val phrase: Array<String> = arrayOf("какоето, значение")
+            // получаем фразы по индексу
+            val word = phrase[1]
+            // присваиваем значение
+            phrase[1] = "другое"
 
-    }
+            // размер присвоить
+            val size = phrase.size
 
+            //-------------------------------
+            // коллекции
+            // Mutable коллекции для изменений
+            // создадим список
+            // mutableList изменяемый
+            class Person(val name: String, var age: Int)
+// хочет var age: сделать String
+            val people: List<Person> = listOf(Person("Макс", "27"), Person("Оля", "25"))
+            //
+            people.[0].age = 26
 
+            // добавить people изменяемый MutableList исп функцию toMutableList()
+            val peopleHack:MutableList<Person> = people.toMutableList()
+            // добавляем нового
+            peopleHack.add(Person("Новый","0"))
 
+            //переменная для
+            var myInt:Int = 2
+            Log.d("myLogs","${myInt.mySguare()}")
+        }
+
+        //---------------------------
+        //своя экстеншен функция для инт
+        fun Int.mySguare():Int{
+            return this*this;
+        }
     }
 
 }
--- app/src/main/java/keyone/keytwo/lesson3_2kotlin/view/MainActivity.kt
+++ app/src/main/java/keyone/keytwo/lesson3_2kotlin/view/MainActivity.kt
@@ -3,6 +3,7 @@ package keyone.keytwo.lesson3_2kotlin.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import keyone.keytwo.lesson3_2kotlin.R
+import keyone.keytwo.lesson3_2kotlin.lesson3.Lesson3
 
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +15,12 @@ class MainActivity : AppCompatActivity() {
         if(savedInstanceState==null)// при повороте сохраняет
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragment_container,MainFragment.newInstance()).commit()
+
+//--------------------------------------
+        // урок 3 массивы
+        val lesson = Lesson3()
+        lesson.mainFirstPart()
+
 
 
     }
